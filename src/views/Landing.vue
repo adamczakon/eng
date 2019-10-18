2110d4add74544d64d8c5a80e6ee49db37d78a9b
Adam <adam.czakon63@gmail.com>
2019-10-19 00:33:10 +0200

    setup routes

---
+++ src/views/Landing.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="test">xd</div>
+</template>
+
+<script>
+export default {
+  name: "landing",
+  components: {}
+};
+</script>
+
+<style scoped lang="scss">
+.test {
+  color: red;
+}
+</style>
